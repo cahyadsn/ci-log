2e8e78e2a0331b9eef2b7c28c0bb8cfcd87f9e06
Cahya DSN <cahyadsn@gmail.com>
2018-01-08 11:22:32 +0700

    update logs library

--- libraries/Logs.php
+++ libraries/Logs.php
@@ -210,4 +210,24 @@ class Logs
             }
         }
     }
+    
+    /**
+    * Get User IP
+    *
+    */
+    function get_user_ip()
+    {
+        $client  = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote  = $_SERVER['REMOTE_ADDR'];
+        $ip='';
+        if (filter_var($client, FILTER_VALIDATE_IP)) {
+            $ip = $client;
+        } elseif(filter_var($forward, FILTER_VALIDATE_IP)) {
+            $ip = $forward;
+        } else {
+            $ip = $remote;
+        }
+        return $ip;
+    }
 }
